3582a21530a88eccfb7c364be9a8b20d6d003912
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-05-30 16:56:43 +0700

    ATM projects.c

--- ATM projects.c
+++ ATM projects.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
 int sodu = 20000000;
+int tax=3000;
 void check() {
     printf("So du la %dVND\n", sodu);
 }
 
 int tinhruttien(int a) {
-    sodu -= a;
+    sodu = sodu -a -tax;
     return sodu;
 }
 
 int tinhguitien(int a) {
-    sodu += a;
+    sodu =sodu+ a-tax;
     return sodu;
 }
 
 int main() {
     char choice, exit = 'N';
     int i, l;
-    int sodu_goc=sodu;
+    int sodu_goc= sodu;
     do {
         printf("Xin vui long nhap ma pin (4 so) vao day :");
         scanf("%d", &l);
@@ -38,15 +39,15 @@ int main() {
                     printf("Nhap so tien muon rut: ");
                     scanf("%d", &i);
                     int ketqua = tinhruttien(i);
-                    printf("Ban da chuyen %dVND.So du con %dVND\n", i, ketqua);
+                    printf("Ban da rut %dVND.Tax -3000VND.So du con lai la %dVND\n", i, ketqua);
                     break;
                 }
                 case 'c': {
                     int j;
-                    printf("nhap so tien ban muon gui: ");
+                    printf("Nhap so tien ban muon gui: ");
                     scanf("%d", &j);
                     int ketqua = tinhguitien(j);
-                    printf("Ban da gui %d.So du dc tang len la %dVND\n", j, ketqua);
+                    printf("Ban da gui %d.Tax -3000VND.So du dc tang len la %dVND\n", j, ketqua);
                     break;
                 }
                 case 'd':{
@@ -56,7 +57,7 @@ int main() {
 
             }
         } else {
-            printf("ban nhap sai mat khau.Vui long thu lai.\n");
+            printf("Ban nhap sai mat khau.Vui long thu lai.\n");
         }if (exit!='N'){
         printf("\nQuy khach co muon tro lai khong ?(Y/N) :");
         scanf(" %c", &exit);}
