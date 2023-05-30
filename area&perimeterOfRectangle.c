894ca1b3f5a30434f200c702f6e341e3ecfbd2f0
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-05-30 08:33:48 +0700

    area&perimeterOfRectangle.c

--- area&perimeterOfRectangle.c
+++ area&perimeterOfRectangle.c
@@ -7,12 +7,29 @@ area(int a,int b){
     int s=a*b;
     return s;
 }
-int main(){
-    int i,j;
-    printf("enter length and width :  ");
-    scanf("%d %d",&i,&j);
-    int dientich=area(i,j);
-    printf("Area of rectangle is : %d\n",dientich);
-    int chuvi= perimeter(i,j);
-    printf("Perimeter of rectangle is : %d ",chuvi);
+int main() {
+    int i, j, choice;
+    do {
+        printf("\nEnter length and width :  ");
+        scanf("%d %d", &i, &j);
+
+        if (i > 0 && j > 0) {
+            printf("||Choice 1 if your want calculator Area of rectangle|| \n");
+            printf("||Choice 2 if your want calculator Perimeter of rectangle|| \n");
+            scanf("%d",&choice);
+            switch (choice) {
+                case 1: {
+                    int dientich = area(i, j);
+                    printf("Area of rectangle is : %d\n", dientich);
+                    break;
+                }
+                case 2: {
+                    int chuvi = perimeter(i, j);
+                    printf("Perimeter of rectangle is : %d ", chuvi);
+                    break;
+                }
+            }
+        } else
+            printf("wrong number!!");
+    } while (1);
 }
